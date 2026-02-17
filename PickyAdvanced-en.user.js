70b30ad3b48e2a8ecb92cd2f351398507ef2115f
hooray804 <profs_35_pointer@icloud.com>
2026-02-17 09:50:08 +0900

    Update PickyAdvanced-en.user.js

--- PickyAdvanced-en.user.js
+++ PickyAdvanced-en.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Picky Advanced
 // @namespace    https://github.com/hooray804/Picky
-// @version      2.1
+// @version      2.2
 // @description  Web Element Inspector & CSS Selector Tool with Ad Block
 // @author       hooray804
 // @license      MPL-2.0
