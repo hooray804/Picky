981c94360c28c04c1c6a656502e1263c9434b5e0
hooray804 <profs_35_pointer@icloud.com>
2026-06-27 23:12:59 +0900

    Update PickyAdvanced.user.js

--- PickyAdvanced.user.js
+++ PickyAdvanced.user.js
@@ -1,11 +1,32 @@
 // ==UserScript==
 // @name         Picky Advanced
 // @namespace    https://github.com/hooray804/Picky
-// @version      5.1.1
+// @version      5.1.2
 // @description  Web Element Inspector & CSS Selector Tool with Ad Block
 // @author       hooray804
 // @license      MPL-2.0
 // @match        *://*/*
+// @exclude      *://captcha-api.yandex.ru/*
+// @exclude      *://ya.ru/showcaptcha?
+// @exclude      *://mil.ru/*
+// @exclude      *wikipedia.org*
+// @exclude      *icloud.com*
+// @exclude      *hangouts.google.com*
+// @exclude      *www.facebook.com/plugins/*
+// @exclude      *www.facebook.com/v*/plugins*
+// @exclude      *disqus.com/embed/comments*
+// @exclude      *vk.com/widget*
+// @exclude      *twitter.com/intent/*
+// @exclude      *www.youtube.com/embed/*
+// @exclude      *player.vimeo.com*
+// @exclude      *coub.com/embed*
+// @exclude      *staticxx.facebook.com/connect/xd_arbiter/*
+// @exclude      *vk.com/q_frame*
+// @exclude      *tpc.googlesyndication.com*
+// @exclude      *syndication.twitter.com*
+// @exclude      *platform.twitter.com*
+// @exclude      *notifications.google.com*
+// @exclude      *google.com/recaptcha/*
 // @grant        GM_setValue
 // @grant        GM_getValue
 // @grant        GM_xmlhttpRequest
